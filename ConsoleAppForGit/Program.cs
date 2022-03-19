115670c5febb9b7e9e34a2a062a09fd3afa6fe15
AbstractRedy <redkylo@gmail.com>
2022-03-19 14:50:02 +0300

    Коммит с файлами

---
+++ ConsoleAppForGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет до коммита");
+            Console.ReadKey();
+        }
+    }
+}
